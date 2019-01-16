c130632c26480b8f74c70e925762282f8d2a2c2d
John O'Keefe <jokeefe@weegolobal.com>
2019-01-16 13:06:15 -0500

    initial app setup

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
-  render() {
+import GlobalStyle from './App.GlobalStyle'
+import Header from './components/Header/Header';
+import Navigation from './components/Navigation/Navigation';
+
+
+function App() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <>
+            <GlobalStyle />
+            <Header />
+            <Navigation />
+        </>
     );
-  }
 }
 
 export default App;
---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Navigation = (props) => (
+    <h1>Navigation Component</h1>
+)
+
+export default Navigation
