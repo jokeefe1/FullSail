97c4beaa31082ce445437871b873cdfa6c3192d9
John O'Keefe <jokeefe@weegolobal.com>
2019-01-27 17:12:33 -0500

    updated Card and CardImage styles

--- src/components/Card/Card.style.js
+++ src/components/Card/Card.style.js
@@ -4,5 +4,6 @@ export const ImgContainer = styled.div`
     display: grid;
     grid-template-columns: 1fr 1fr 1fr;
     grid-template-rows: repeat(2, 1fr);
+    grid-row-gap: 5rem;
 
 `;
--- src/components/CardImage/CardImage.style.js
+++ src/components/CardImage/CardImage.style.js
@@ -2,35 +2,40 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 export const StyledLink = styled(Link)`
-           text-decoration: none;
-           & > div:nth-child(1) {
-               bottom: 0px;
-               height: 25rem;
-               background-size: cover;
-               background-repeat: no-repeat;
-               position: relative;
-           }
-           & > div > h3 {
-               background-color: ${({ theme }) => theme.yellow};
-               padding: 1.5rem 3rem;
-               text-transform: uppercase;
-               color: ${({ theme }) => theme.white};
-               position: absolute;
-               bottom: 0;
-           }
-           & > div > p {
-               display: flex;
-               justify-content: space-around;
-               align-items: center;
-               background-color: ${({ theme }) => theme.grey};
-               color: ${({ theme }) => theme.lightGrey};
-               padding: 2rem;
-               font-size: 2rem;
-               height: 10rem;
-               & > span {
-                   color: ${({ theme }) => theme.yellow};
-                   font-size: 5rem;
-                   padding-right: 1rem;
-                   text-decoration: none;
-               }
-           }`;
+    text-decoration: none;
+    background-color: ${({theme}) => theme.white};
+    & > div:nth-child(1) {
+        bottom: 0px;
+        height: 25rem;
+        background-size: cover;
+        background-repeat: no-repeat;
+        position: relative;
+    }
+    & > div > h3 {
+        background-color: ${({ theme }) => theme.yellow};
+        padding: 1.5rem 3rem;
+        text-transform: uppercase;
+        color: ${({ theme }) => theme.white};
+        position: absolute;
+        bottom: 0;
+    }
+    & > div > p {
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        background-color: ${({ theme }) => theme.darkWhite};
+        color: ${({ theme }) => theme.lightGrey};
+        padding: 2rem;
+        font-size: 2rem;
+        height: 10rem;
+        border-left: 0.1rem solid ${({ theme }) => theme.grey};
+        border-bottom: 0.1rem solid ${({ theme }) => theme.grey};
+
+        & > span {
+            color: ${({ theme }) => theme.yellow};
+            font-size: 5rem;
+            padding-right: 1rem;
+            text-decoration: none;
+        }
+    }
+`;
