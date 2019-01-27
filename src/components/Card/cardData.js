f0c3c69f0d826e7b217159181148e874abc84b57
John O'Keefe <jokeefe@weegolobal.com>
2019-01-27 13:03:58 -0500

    Initial Card

---
+++ src/components/Card/cardData.js
@@ -0,0 +1,7 @@
+
+export const cardData = {
+    id: '1'
+    image: 'https://res.cloudinary.com/john-okeefe/image/upload/v1547931614/fullsail/homepage_1.jpg',
+    title: 'This is the title',
+    label: 'This is the label that makes us unique'
+}
