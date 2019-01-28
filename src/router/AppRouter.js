89c39221f291502ba3a92f7bc9a5fe0eccf671ab
John O'Keefe <jokeefe@weegolobal.com>
2019-01-28 12:49:58 -0500

    removed unused routes

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -8,9 +8,6 @@ const AppRouter = () => (
     <BrowserRouter>
             <Switch>
                 <Route path="/" component={HomePage} exact />
-                <Route path="/info" component={NotFound} exact />
-                <Route path="/chat" component={NotFound} exact />
-                <Route path="/apply" component={NotFound} exact />
                 <Route component={NotFound} />
             </Switch>
     </BrowserRouter>
