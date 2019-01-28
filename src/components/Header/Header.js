21a9fdd228cee1b7bfdb5b401e8a9cbd39cf8c4f
John O'Keefe <jokeefe@weegolobal.com>
2019-01-27 23:47:06 -0500

    initial drawer and styles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,36 +1,42 @@
 import React from 'react';
 import { FaInfo, FaPencilAlt, FaRocketchat } from 'react-icons/fa';
 import { ContainerHead } from '../../App.GlobalStyle';
-import { ImgDiv, NavBar, NavLinks, StyledLink, MobileMenu } from './Header.style';
-import { MdMenu } from 'react-icons/md'
+import {
+    ImgDiv,
+    MobileMenu,
+    NavBar,
+    NavLinks,
+    StyledLink
+} from './Header.style';
+import TemporaryDrawer from '../TemporaryDrawer/TemporaryDrawer'
 
 
-const Header = props => (
-    <NavBar>
-        <ContainerHead>
-            <ImgDiv>
-                <img
-                    src="https://res.cloudinary.com/john-okeefe/image/upload/v1547931614/fullsail/logo.svg"
-                    alt="logo"
-                />
-            </ImgDiv>
-            <NavLinks>
-                <StyledLink to="/info">
-                    Request Info <FaInfo />
-                </StyledLink>
-                <StyledLink to="/chat">
-                    Chat Live <FaRocketchat />
-                </StyledLink>
-                <StyledLink to="/apply">
-                    Apply <FaPencilAlt />
-                </StyledLink>
-                <StyledLink to="#">800.226.7625</StyledLink>
-            </NavLinks>
-            <MobileMenu>
-                <MdMenu />
-            </MobileMenu>
-        </ContainerHead>
-    </NavBar>
-);
-
-export default Header;
+export default function Header() {
+  return (
+      <NavBar>
+          <ContainerHead>
+              <ImgDiv>
+                  <img
+                      src="https://res.cloudinary.com/john-okeefe/image/upload/v1547931614/fullsail/logo.svg"
+                      alt="logo"
+                  />
+              </ImgDiv>
+              <NavLinks>
+                  <StyledLink to="/info">
+                      Request Info <FaInfo />
+                  </StyledLink>
+                  <StyledLink to="/chat">
+                      Chat Live <FaRocketchat />
+                  </StyledLink>
+                  <StyledLink to="/apply">
+                      Apply <FaPencilAlt />
+                  </StyledLink>
+                  <StyledLink to="#">800.226.7625</StyledLink>
+              </NavLinks>
+              <MobileMenu>
+              </MobileMenu>
+          </ContainerHead>
+          <TemporaryDrawer />
+      </NavBar>
+  )
+}
