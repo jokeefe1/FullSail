b510bafff8c73d8659289f9122970f4e7b0f921f
John O'Keefe <jokeefe@weegolobal.com>
2019-01-31 14:39:25 -0500

    added AWS Storage and Analytics

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -8,7 +8,11 @@ const awsmobile =  {
     "aws_cognito_identity_pool_id": "us-east-1:c56a497c-2926-436b-81ff-7137cb362f48",
     "aws_cognito_region": "us-east-1",
     "aws_user_pools_id": "us-east-1_IjqGzE2sq",
-    "aws_user_pools_web_client_id": "l6v4814dg0uic7g9vanpl4kk4"
+    "aws_user_pools_web_client_id": "l6v4814dg0uic7g9vanpl4kk4",
+    "aws_mobile_analytics_app_id": "84b7f0272be149d3b4ba3297238d57d6",
+    "aws_mobile_analytics_app_region": "us-east-1",
+    "aws_user_files_s3_bucket": "fullsail9f9a98ffc72f460185f3bce178c45e85",
+    "aws_user_files_s3_bucket_region": "us-east-1"
 };
 
 
--- src/components/Student/Student.js
+++ src/components/Student/Student.js
@@ -1,13 +1,40 @@
-import React from 'react'
-import Amplify from 'aws-amplify';
+import Amplify, { Analytics, Storage } from 'aws-amplify';
+import { PhotoPicker, S3Album, withAuthenticator } from 'aws-amplify-react';
+import React from 'react';
 import aws_exports from '../../aws-exports';
-import { withAuthenticator } from 'aws-amplify-react';
 Amplify.configure(aws_exports);
+Storage.configure({ level: 'private' });
 
-function Student() {
-    return (
-        <h1>Hello you are now in the secured student area</h1>
-    )
+class Student extends React.Component {
+    uploadFile = evt => {
+        const file = evt.target.files[0];
+        const name = file.name;
+
+        Storage.put(name, file).then(() => {
+            this.setState({ file: name });
+        });
+    };
+
+    componentDidMount() {
+        Analytics.record('Amplify_CLI');
+    }
+
+    render() {
+        return (
+            <>
+                <h1>Hello you are now in the secured student area</h1>
+                <div className="App">
+                    <p> Pick a file</p>
+                    <input type="file" onChange={this.uploadFile} />
+                    <S3Album level="private" path="" />
+                    <PhotoPicker
+                        preview
+                        onLoad={dataURL => console.log(dataURL)}
+                    />
+                </div>
+            </>
+        );
+    }
 }
 
 export default withAuthenticator(Student, true);
